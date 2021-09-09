b3908713ec15c303e65408c0587f8ed34a50d439
Mike-Skvortsov <mixaylo.skv@gmail.com>
2021-09-09 15:45:38 +0300

    +

---
+++ laba2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lab2
+{
+    class HelloWorld
+    {
+        //lab2
+        static void Main()
+        {
+            int nk;
+            int nn;
+            double a;
+            double result = 1;
+            do
+            {
+                Console.WriteLine("input nn : ");
+                nn = Convert.ToInt32(Console.ReadLine());
+            } while (nn <= 0);
+            do
+            {
+                Console.WriteLine("input nk : ");
+                nk = Convert.ToInt32(Console.ReadLine());
+            } while (nk <= nn);
+            double k = nn;
+            for (int i = nk; i >= nn; i--)
+            {
+                a = 0;
+                a = (Math.Pow(-1, k) * Math.Pow(k, 2) + Math.Pow(-1, (Math.Pow(k, 2) + 1)) * k) / (2 * (Math.Pow(k, 3) + 3));
+                result *= a;
+                if (k <= nk)
+                {
+                    k++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ laba3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		//Перевірити істинність вислову: "Дані числа x, у є координатами точки, що лежить у другому квадранті ".
+		static void Main()
+		{
+			float x, y;
+			Console.WriteLine("введiть х : ");
+			x = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("введiть у : ");
+			y = Convert.ToInt32(Console.ReadLine());
+			if (x <= 0 && y >= 0)
+			{
+				Console.WriteLine("True");
+			}
+			else
+				Console.WriteLine("False");
+		}
+	}
+}
