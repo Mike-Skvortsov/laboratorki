697f5d77a8b4e5aae76d9dcc2154f03c9ff4ac44
Mike-Skvortsov <mixaylo.skv@gmail.com>
2021-09-11 21:23:23 +0300

    lab4   +

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -8,8 +8,28 @@ namespace lab4
 {
 	class Program
 	{
-		static void Main(string[] args)
+		//Даний масив розміру N. Перетворити його, вставивши перед (після) кожного додатного (від’ємного) елементу нульовий елемент.
+		static void Main()
 		{
+			int size = 0;
+			Console.Write("Введiть розмiр масиву : ");
+			size = int.Parse(Console.ReadLine());
+			double[] arr = new double [size];
+			for (int i = 0; i < size; i++)
+			{
+				Console.Write(i + 1 + " елемент масиву : ");
+				arr[i] = int.Parse(Console.ReadLine());
+				if (i > 0 && arr[i - 1] > 0)
+				{
+					arr[i] = 0;
+				}	
+
+			}
+			Console.WriteLine("______________________________");
+			for (int i = 0; i < size; i++)
+			{
+				Console.WriteLine(i + 1 + " елемент масиву : " + arr[i]);
+			}
 		}
 	}
 }
